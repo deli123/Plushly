6bce1d8efe8d4551beaa4bce432241da594d10b0
Derek <liderek63@gmail.com>
2023-10-15 01:46:20 -0700

    add header for query

--- scripts/search.js
+++ scripts/search.js
@@ -1,14 +1,25 @@
 const searchField = document.getElementById("product-search");
 
-const myFunction = () => {
+const searchFunction = () => {
   let input = searchField.value.toLowerCase();
+  let searchTitle = document.getElementById("search-title");
+
+  // Change the header text based on input
+  if (input) {
+    searchTitle.innerText = `Search results for "${input}"`;
+    searchTitle.classList.add("mt-3");
+    searchTitle.classList.remove("d-none");
+  } else {
+    searchTitle.innerText = "";
+    searchTitle.classList.remove("mt-3");
+  }
 
   const cardTitles = document.getElementsByClassName("card-title");
-
+  
   // Convert the HTMLCollection to an array for easier manipulation
   let cardTitlesArray = Array.from(cardTitles);
 
-  cardTitlesArray.forEach(function (title) {
+  cardTitlesArray.forEach((title) => {
     // Check if the title contains the filter text
     let titleText = title.innerText.toLowerCase();
     if (titleText.includes(input)) {
